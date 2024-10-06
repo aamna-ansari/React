e4b566a711388e6187674a3abc832f841c652eeb
aamna-ansari <aamnansari29@gmail.com>
2024-10-06 10:32:21 +0500

    👩‍💻

---
+++ React/01_basic_react/src/Me.js
@@ -0,0 +1,7 @@
+function Me(){
+    return ( 
+    <h1>I'm AaMna AnSari</h1>
+);
+}
+
+export default Me;
--- React/01_basic_react/src/index.js
+++ React/01_basic_react/src/index.js
@@ -3,12 +3,12 @@ import React from 'react'; //core foundation library
 // DOM => is tree
 import './index.css';
 import App from './App';
+import Me from './Me' //Create a new Component jusst for practice 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App />,
+    <Me/>
+
 );
 
-reportWebVitals();
