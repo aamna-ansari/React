f3a8ed1ea513706e0491ac5d605633f2d3e0f502
aamna-ansari <aamnansari29@gmail.com>
2024-10-07 22:55:46 +0500

    counter

--- React/02_counter/src/App.jsx
+++ React/02_counter/src/App.jsx
@@ -1,16 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let counter = 5
+
+  let [counter, setCounter] = useState(15)
+
+  // let counter = 5
+
+  // function for add value
+  const addValue = () => {
+    counter = counter + 1
+    setCounter(counter)
+    console.log('clicked added', counter);
+  }
+  const removeValue = () => {
+    counter = counter - 1
+    setCounter(counter)
+    console.log('clicked added', counter);
+  }
+  
+
   return (
     <>
       <h1>AaMna AnSari </h1>
       <h2>Counter Value: {counter}</h2>
-      <button>Add Value: {counter}</button>
-      <button>Delete Value:{counter}</button>
+      <button onClick={addValue}>Add Value: {counter}</button>
+      <button onClick={removeValue}>Delete Value:{counter}</button>
     </>
   )
 }
