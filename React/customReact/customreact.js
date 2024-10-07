8f42b8da84c2df158fd4d554ea3337f90d5b81fe
aamna-ansari <aamnansari29@gmail.com>
2024-10-07 21:04:15 +0500

    DOM

--- React/customReact/customreact.js
+++ React/customReact/customreact.js
@@ -1,3 +1,19 @@
 // grip the root by DOM 
 
 let mainContainer = document.querySelector("#root")
+
+
+// customRender function
+
+
+// Render 'a' tag
+let reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Clicked me to visit Google 😉'
+}
+
+customRender(reactElement, mainContainer)
