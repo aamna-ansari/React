276ac24ea3d31317b07b70451aae293404227cb3
aamna-ansari <aamnansari29@gmail.com>
2024-10-07 21:49:40 +0500

    ReactElement

--- React/01_vite_react_project/src/main.jsx
+++ React/01_vite_react_project/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+// Create React element
+
+let anotherElement = React.createElement(
+ 'a',
+ {href: 'https://google.com', target: '_blank'
+},
+ 'Click me to visit google'
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  anotherElement
+  
+ 
 )
 
