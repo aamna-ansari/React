99b5e13af4070039f429b6c9b2270f441acaae3e
aamna-ansari <aamnansari29@gmail.com>
2024-10-08 22:06:04 +0500

    .

--- React/03_tailwind_and_props/src/App.jsx
+++ React/03_tailwind_and_props/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 p-4 text-black rounded-xl mb-5' >Tailwind Test</h1>
-          
+        <Card/>  
     </>
   )
 }
