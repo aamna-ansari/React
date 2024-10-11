5744d998e9627ecbb934ad06b2aad2c6e39ea2e9
aamna-ansari <aamnansari29@gmail.com>
2024-10-11 20:13:05 +0500

    added

--- React/04_bgColor/src/App.jsx
+++ React/04_bgColor/src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 
 function App() {
- 
+ const [color, setColor] = useState('olive')
 
   return (
     
       <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
-        <div className='fixed flex felx-wrap justify-center bottom-12 inset-x-0 px-2'></div>
+        <div className='fixed flex felx-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white'>
+            <button className='outline-none px-4' style={{backgroundColor: 'red'}}>Red</button>
+          </div>
+        </div>
       </div>
     
   )
