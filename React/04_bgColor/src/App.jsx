213e6a758f5e828258e2482f9d1a4178c67a32b6
aamna-ansari <aamnansari29@gmail.com>
2024-10-11 20:23:51 +0500

    YahooDone

--- React/04_bgColor/src/App.jsx
+++ React/04_bgColor/src/App.jsx
@@ -7,8 +7,14 @@ function App() {
     
       <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
         <div className='fixed flex felx-wrap justify-center bottom-12 inset-x-0 px-2'>
-          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white'>
-            <button className='outline-none px-4' style={{backgroundColor: 'red'}}>Red</button>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor: 'red'}}>Red</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor: 'green'}}>Green</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor: 'navy'}}>Navy</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor: 'deeppink'}}>Deeppink</button>
+            <button className='outline-none px-4 py-1 rounded-full text-black' style={{backgroundColor: 'skyblue'}}>Skyblue</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor: 'black'}}>Black</button>
+            <button className='outline-none px-4 py-1 rounded-full text-white' style={{backgroundColor: 'maroon'}}>Maroon</button>
           </div>
         </div>
       </div>
