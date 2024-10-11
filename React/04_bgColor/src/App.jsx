3c170c3e4a7a3f0fd850a60416bd0359dcd19d48
aamna-ansari <aamnansari29@gmail.com>
2024-10-11 19:58:58 +0500

    1added

--- React/04_bgColor/src/App.jsx
+++ React/04_bgColor/src/App.jsx
@@ -4,9 +4,11 @@ function App() {
  
 
   return (
-    <>
-      
-    </>
+    
+      <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
+        <div className='fixed flex felx-wrap justify-center bottom-12 inset-x-0 px-2'></div>
+      </div>
+    
   )
 }
 
