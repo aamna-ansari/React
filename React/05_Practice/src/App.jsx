dbbbfcaa9e42359afb7c209b5b57d3ea353de81a
aamna-ansari <aamnansari29@gmail.com>
2024-10-31 15:28:14 +0500

    FormHandling

--- React/05_Practice/src/App.jsx
+++ React/05_Practice/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 
 export const App = () => {
+const submitHandler = (e)=>{
+  e.preventDefault()
+  console.log('Submitted');
+  
+}
   return (
-    <div>
-      <h1 className='text-5xl text-center font-semibold'>Helo AaMna</h1>
+    <div >
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }} >
+        <input className='px-4 py-4 rounded m-5 text-black  ' type="text" placeholder='Enter Your Name' />
+        <button className ='px-4 py-4 m-3 rounded text-2xl bg-gray-400' type="submit">Submit</button>
+      </form>
     </div>
   )
 } 
