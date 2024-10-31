6ae89eeb8dae40b1c77cd06eb4063c383840fa7a
aamna-ansari <aamnansari29@gmail.com>
2024-10-31 19:13:12 +0500

    HeaderComponent created

--- React/05_Practice/src/App.jsx
+++ React/05_Practice/src/App.jsx
@@ -1,30 +1,55 @@
 import React, { useState } from 'react';
+import Header from "./Components/Header";
+
+// export const App = () => {
+//   const [username, setUsername] = useState(''); // username = ''
+
+//   const submitHandler = (e) => {
+//     e.preventDefault();
+//     console.log(username);
+//     setUsername(''); // Clears the input field after submission
+//   };
+
+//   return (
+//     <div>
+//       <form onSubmit={submitHandler}>
+//         <input
+//           className="px-4 py-4 rounded m-5 text-black"
+//           type="text"
+//           placeholder="Enter Your Name"
+//           value = {username} // Bind input value to the state
+//           onChange={(e) => setUsername(e.target.value)} // Update state on input change
+//         />
+//         <button className="px-4 py-4 m-3 rounded text-2xl bg-gray-400" type="submit">
+//           Submit
+//         </button>
+//       </form>
+//     </div>
+//   );
+// };
+
+// COMPONTENTS
 
 export const App = () => {
-  const [username, setUsername] = useState(''); // username = ''
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(username);
-    setUsername(''); // Clears the input field after submission
+    return (
+      <>
+      
+      <nav className='bg-emerald-950 flex items-center justify-between py-4 px-2'>
+      <h1 className='text-2xl'>AaMna</h1>
+      <div className='flex items-center gap-10'>
+        <h4 className='text-xl'>About</h4>
+        <h4 className='text-xl'>Contact</h4>
+        <h4 className='text-xl'>Services</h4>
+        <h4 className='text-xl'>Account</h4>
+      </div>
+    </nav>
+    <Header/>
+      </>
+    );
+      
+      
   };
 
-  return (
-    <div>
-      <form onSubmit={submitHandler}>
-        <input
-          className="px-4 py-4 rounded m-5 text-black"
-          type="text"
-          placeholder="Enter Your Name"
-          value = {username} // Bind input value to the state
-          onChange={(e) => setUsername(e.target.value)} // Update state on input change
-        />
-        <button className="px-4 py-4 m-3 rounded text-2xl bg-gray-400" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+
 
 export default App;
---
+++ React/05_Practice/src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>I'm Header</div>
+  )
+}
+
+export default Header
