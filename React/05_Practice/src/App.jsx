d24784a8c596adb4842ccd01e5711380776b768f
aamna-ansari <aamnansari29@gmail.com>
2024-10-31 15:13:08 +0500

    added

---
+++ React/05_Practice/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl text-center font-semibold'>Helo AaMna</h1>
+    </div>
+  )
+} 
+export default App
