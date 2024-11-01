749a5be3368a2497834b89efc9d0e1a2c70513df
aamna-ansari <aamnansari29@gmail.com>
2024-11-01 13:16:21 +0500

    06

--- React/05_Practice/src/App.jsx
+++ React/05_Practice/src/App.jsx
@@ -33,21 +33,9 @@ import Header from "./Components/Header";
 export const App = () => {
     return (
       <>
-      
-      <nav className='bg-emerald-950 flex items-center justify-between py-4 px-2'>
-      <h1 className='text-2xl'>AaMna</h1>
-      <div className='flex items-center gap-10'>
-        <h4 className='text-xl'>About</h4>
-        <h4 className='text-xl'>Contact</h4>
-        <h4 className='text-xl'>Services</h4>
-        <h4 className='text-xl'>Account</h4>
-      </div>
-    </nav>
     <Header/>
       </>
     );
-      
-      
   };
 
 
--- React/05_Practice/src/Components/Header.jsx
+++ React/05_Practice/src/Components/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>I'm Header</div>
+    <nav className='bg-emerald-950 flex items-center justify-between py-4 px-2'>
+      <h1 className='text-2xl'>AaMna</h1>
+      <div className='flex items-center gap-10'>
+        <h4 className='text-xl'>About</h4>
+        <h4 className='text-xl'>Contact</h4>
+        <h4 className='text-xl'>Services</h4>
+        <h4 className='text-xl'>Account</h4>
+      </div>
+    </nav>
   )
 }
 
