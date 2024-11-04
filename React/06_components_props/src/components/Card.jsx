496b5ee22e53dcceae56492c8a06fee7e9902efe
aamna-ansari <aamnansari29@gmail.com>
2024-11-04 21:33:05 +0500

    .

--- React/06_components_props/src/components/Card.jsx
+++ React/06_components_props/src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
         <img className='ml-8 h-32 w-32 rounded-full mb-3' src={props.profile_photo} alt="Profile" />
         <h1 className='text-2xl font-semibold mb-4'>{props.username}</h1>
         <h2>{props.profession}</h2>
-        <button className='bg-emerald-700 text-white px-4 py-2 rounded font-medium' >Add Friend</button>
+        <button className='bg-emerald-700 text-white px-5 py-2 rounded font-medium' >Add Friend</button>
     </div>
     </>
   )
