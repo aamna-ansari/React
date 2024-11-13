4b39aaca59650e7c5aecbaa607439b4d65c807f5
aamna-ansari <aamnansari29@gmail.com>
2024-11-13 20:18:10 +0500

    Button Added

--- react2/class02/src/pages/frontend/Home.jsx
+++ react2/class02/src/pages/frontend/Home.jsx
@@ -8,6 +8,16 @@ const Home = () => {
           <div className="col">
             <div className='py-5'>
               <h1 className='mb-0 text-center'>This is Learning React Home Page with Bootstrap </h1>
+              <div className='p-5'>
+              <button className='btn btn-primary '>primary button</button>
+              <button className='btn btn-secondary'>secondary button</button>
+              <button className='btn btn-success'>success button</button>
+              <button className='btn btn-danger'>danger button</button>
+              <button className='btn btn-warning'>warning button</button>
+              <button className='btn btn-info'>info button</button>
+              <button className='btn btn-dark'>dark button</button>
+              <button className='btn btn-light'>light button</button>
+              </div>
             </div>
           </div>
         </div>
