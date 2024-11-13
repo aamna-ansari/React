931fdc99ad4a7319238e439e4fa6bd59e165bf2b
aamna-ansari <aamnansari29@gmail.com>
2024-11-13 21:58:26 +0500

    folder structure done

---
+++ react2/class03/README.md
@@ -0,0 +1,8 @@
+# React_Class-03
+
+### What I Learn:
+
+- JSX
+- fragments
+- Event Handling
+
---
+++ react2/class03/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.scss'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center text-primary'>Helo World</h1>
+      </div>
+  )
+}
+
+export default App
