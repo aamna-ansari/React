ddd9b549637e2985f50f05ca1cf6cb3c66f2537c
aamna-ansari <aamnansari29@gmail.com>
2024-11-13 21:36:10 +0500

    Class02 practice Done(create to map)

---
+++ react2/class02/README.md
@@ -0,0 +1,16 @@
+# React_Class-02 
+
+### What i learn:
+
+- create project
+- install sass & bootstrap  ``` npm i sass bootstrap```
+- folder structure 
+- compontents
+- props
+- destructuring props
+- ternary Operator 
+- template literal
+- map()
+
+![](./class02.PNG)
+
--- react2/class02/src/pages/frontend/Home.jsx
+++ react2/class02/src/pages/frontend/Home.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 import Button from '../../components/Button'
 
 const Home = () => {
+const students = [
+  { firstName: "Alice", grade: "A", age: 15 },
+  { firstName: "Bob", grade: "B", age: 16 },
+  { firstName: "Charlie", grade: "A", age: 17 },
+  { firstName: "Diana", grade: "C", age: 15 },
+  { firstName: "Evan", grade: "B", age: 16 }
+  
+];
+
   return (
     <main> 
       <div className="container">
@@ -9,13 +18,44 @@ const Home = () => {
           <div className="col">
             <div className='py-5'>
               <h1 className='mb-0 text-center'>This is Learning React Home Page with Bootstrap </h1>
-              <div className='p-5'>
-              <Button type="primary" text = 'primary' /> 
-              <Button type="danger" text = 'danger' /> 
-              <Button type="success" text = 'success' /> 
+              <div className='p-5 text-center'>
               <Button/> 
+              <Button type="danger" text = 'danger Button' /> 
+              <Button type="success" text = 'success Button' /> 
+              <Button type="secondary" text = 'secondary Button' /> 
+              <Button type="dark" text = 'dark Button' /> 
+              <Button type="light" text = 'light Button' /> 
+              <Button type="info" text = 'info Button' /> 
+              <Button type="warning" text = 'warning Button' /> 
               {/* <button className='btn btn-light'>light button</button> */} 
               </div>
+              <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">FirstName</th>
+      <th scope="col">Grade</th>
+      <th scope="col">Age</th>
+    </tr>
+  </thead>
+  <tbody>
+    {students.map((student, ind)=>{
+      return (
+    <tr>
+      <th>{ind+1}</th>
+      <td>{student.firstName}</td>
+      <td>{student.grade}</td>
+      <td>{student.age}</td>
+    </tr>
+
+      )
+
+    })}
+    
+    
+  </tbody>
+              </table>
+
             </div>
           </div>
         </div>
