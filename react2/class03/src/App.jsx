7f28096b9caf139497c02ae2a01c89b47a2d7c76
aamna-ansari <aamnansari29@gmail.com>
2024-11-18 21:57:15 +0500

    react-router-dom'

--- react2/class03/src/App.jsx
+++ react2/class03/src/App.jsx
@@ -3,12 +3,16 @@ import './App.scss'
 import 'bootstrap/dist/js/bootstrap.bundle'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import {Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <>
-      {/* <About/> */}
-      <Contact/>
+    
+    <Routes>
+    <Route path= '/' element ={<Contact/>}/>
+    <Route path= '/about' element ={<About/>}/>
+    </Routes>
     </>
   )
 }
