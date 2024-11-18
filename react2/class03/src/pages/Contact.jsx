a67948811bd1f7f981835de4d42520905b6585ca
aamna-ansari <aamnansari29@gmail.com>
2024-11-18 21:36:17 +0500

    Hook

--- react2/class03/src/pages/Contact.jsx
+++ react2/class03/src/pages/Contact.jsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react'
 
 const Contact = () => {
-    let [name] = useState('')
+   const [name, setName] = useState('') //Hooks useState
+   const [color, setColor] = useState('#fff') //Hooks useState
    let name1 = '';
    let city = '';
     
@@ -11,16 +12,19 @@ const Contact = () => {
         console.log("City", city);
     }
   return (
-    <main>
+    <main style={{backgroundColor: color}}>
     <div className="container">
         <div className="row">
             <div className="col">
                 <div className="py-5">
                     <h1 className='text-center mb-5'>Learn React-Form with AaMna AnSari </h1>
+                    <h1 className='text-center mb-5'>{name} </h1>
+                    <input type="color" onChange={e => {setColor(e.target.value)}} />
                     <form>
                         <div className="row">
                             <div className="col-12 col-md-6 mb-5">
-                                <input type="text" placeholder='Enter your name:' className='form-control' onChange={(e)=>{name = e.target.value;
+                                {/* <input type="text" placeholder='Enter your name:' className='form-control' onChange={(e)=>{name = e.target.value; */}
+                                <input type="text" placeholder='Enter your name:' className='form-control' onChange={(e)=>{setName(e.target.value)
                                 }}/>
                             </div>
                             <div className="col-12 col-md-6 mb-5">
