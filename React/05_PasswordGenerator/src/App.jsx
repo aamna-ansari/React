09238d908e9943e9d482e798eb0f3a13e2f32265
aamna-ansari <aamnansari29@gmail.com>
2024-12-01 20:26:36 +0500

    Math.random

--- React/05_PasswordGenerator/src/App.jsx
+++ React/05_PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import './App.css'
 
 function App() {
@@ -7,9 +7,18 @@ function App() {
   const [charAllowed , setCharAllowed] = useState(false)
   const [numberAllowed , setNumberAllowed] = useState(false)
 
-  const passwordGenerator = ()=>{
-    
-  }
+  const passwordGenerator = useCallback(() => {
+    let pass = ''
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+    if (numberAllowed) str += '0123456789'
+    if (charAllowed) str += '$%*(){}+&!^*'
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length +1)
+      
+    }
+  }, [length, charAllowed, numberAllowed, setPassword])
   return (
     <>
       <h1 className='text-4xl text-center' >Password Generator</h1>
